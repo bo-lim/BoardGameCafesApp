cfc64419e0a4b9b1c429cacc66aad8d1fe3696e7
Alice <jaeyeon0921@gmail.com>
2024-02-21 02:57:12 +0900

    Add search button to main page

--- dice-client-app/src/app/page.tsx
+++ dice-client-app/src/app/page.tsx
@@ -1,14 +1,55 @@
-import { Flex, Text, Button } from "@radix-ui/themes";
-
+"use client";
+import {
+  Flex,
+  TextField,
+  Button,
+  Box,
+  Callout,
+  Heading,
+  Select,
+} from "@radix-ui/themes";
+import { MagnifyingGlassIcon, InfoCircledIcon } from "@radix-ui/react-icons";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Flex direction="column" gap="2">
-        <Text>Welcome to DICE</Text>
-        <Button size="2" variant="soft">
-          로그인
-        </Button>
+    <>
+      <Flex direction={"column"} gap={"8"}>
+        <Heading as="h1" className="text-center">
+          보드게임 종합 검색
+        </Heading>
+        <Callout.Root>
+          <Callout.Icon>
+            <InfoCircledIcon />
+          </Callout.Icon>
+          <Callout.Text>
+            현재 검색 가능한 필터들은 아래와 같습니다. 뭔가를 바라시겠지만,
+            {"\n"}
+            이것이 우리의 한계! 국비 끝나면 업데이트 할수도? 안할수도.. 헿
+            <ul>
+              <li>- 보드게임 카페 이름</li>
+              <li>- 보드게임</li>
+              <li>- 지하철역</li>
+            </ul>
+          </Callout.Text>
+        </Callout.Root>
+        <Flex gap="1" className="flex-col sm:flex-row">
+          <TextField.Root>
+            <TextField.Slot>
+              <MagnifyingGlassIcon height="16" width="16" />
+            </TextField.Slot>
+            <TextField.Input size="3" placeholder="검색" />
+          </TextField.Root>
+          <Select.Root defaultValue="game" size="3">
+            <Select.Trigger />
+            <Select.Content>
+              <Select.Item value="game">보드게임</Select.Item>
+              <Select.Item value="cafe">보드게임 카페</Select.Item>
+              <Select.Item value="metro">지하철역</Select.Item>
+            </Select.Content>
+          </Select.Root>
+          <Button size="3">검색</Button>
+        </Flex>
+        <Box className="mt-10">test</Box>
       </Flex>
-    </main>
+    </>
   );
 }
