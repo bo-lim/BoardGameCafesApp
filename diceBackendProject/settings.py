665bdd011cc4e99a9cfd9b812571604f38a3682a
bo-lim <leebolim701@cau.ac.kr>
2024-02-20 11:12:18 +0900

    @bo-lim [feature]finish user migration

--- diceBackendProject/settings.py
+++ diceBackendProject/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # APPs
-    # 'user',
+    'user',
     # Library
     'rest_framework',
     'rest_framework_simplejwt',
@@ -142,7 +142,7 @@ SIMPLE_JWT = {
 }
 
 # user
-# AUTH_USER_MODEL = 'user.User'
+AUTH_USER_MODEL = 'user.User'
 
 
 # Password validation
--- user/models.py
+++ user/models.py
@@ -1,14 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-'''
-class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length=3-50, unique=True, null=False, blank=False)
-    userName = models.CharField(max_length=30)
-    #password = 
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    objects = UserManager()
-    USERID_FIELD = 'email'
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password, **kwargs):
@@ -21,4 +12,14 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
-'''
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=50, unique=True, null=False, blank=False)
+    userName = models.CharField(max_length=30)
+    #password = 
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+
+
