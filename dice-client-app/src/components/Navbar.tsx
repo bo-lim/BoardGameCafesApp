11f96f4908b1c71020ad865baf2726344ffeb7d0
Alice <jaeyeon0921@gmail.com>
2024-02-21 02:57:37 +0900

    Update navbar

--- dice-client-app/src/components/Navbar.tsx
+++ dice-client-app/src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Heading, Text, Button, IconButton } from "@radix-ui/themes";
+import { Heading, Button, IconButton } from "@radix-ui/themes";
 import { RowsIcon } from "@radix-ui/react-icons";
 const Navbar = () => {
   return (
     <>
       <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 dark:border-gray-600">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-8 sm:px-20 py-4">
           <a
             href="/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
@@ -19,8 +19,8 @@ const Navbar = () => {
             </Button>
           </a>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <Button>로그인</Button>
-            <IconButton variant="outline">
+            <Button size="3">로그인</Button>
+            <IconButton variant="outline" size={"3"} className="xs:invisible">
               <RowsIcon />
             </IconButton>
           </div>
