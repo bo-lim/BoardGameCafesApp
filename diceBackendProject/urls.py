110116963e0df60f55c2f7b59077f515593b7748
bo-lim <leebolim701@cau.ac.kr>
2024-02-22 19:43:14 +0900

    [feature] fix model & make register api:

--- diceBackendProject/urls.py
+++ diceBackendProject/urls.py
@@ -19,6 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
     path('user/', include('user.urls'))
 ]
