2e6a57e0d7329212dd06ac7ef9c74b8b151f3a0d
bo-lim <leebolim701@cau.ac.kr>
2024-02-22 20:16:28 +0900

    [feature]login logout

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 from .views import RegisterView, AuthView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view()),
-    path('auth/', AuthView.as_view())
+    path('register', RegisterView.as_view()),
+    path('auth', AuthView.as_view())
 ]
